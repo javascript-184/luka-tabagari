a53efea24a942d8011cf77cf681d64cfe85430b2
LukaTabagari2222 <tabagariluka32@gmail.com>
2025-01-04 00:40:10 +0400

    l finish assignment4' 4 exercize

--- davalebaN4_switch.js/voice.js
+++ davalebaN4_switch.js/voice.js
@@ -1,1 +1,15 @@
-alert("aklsj")
+// let person =prompt("plis enters you age");
+// if(person=Number(person)){
+//     alert(`${person>=18} You can vote!`)
+// }else(
+//     alert(`${person} You cannot vote!`)
+// )
+// let person = prompt("plis enters you age");
+// switch (person = Number(person)>=18) {
+//     case person>=18:
+//         alert(`${person} you can not vote 🥵 `)
+//         break
+//     default:
+//         alert(`${person<18} You can vote!😀 `)
+
+// }
