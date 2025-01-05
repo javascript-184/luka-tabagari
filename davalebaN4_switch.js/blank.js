2caf24f3463a4f22c3f64c0319e57ea72763362a
LukaTabagari2222 <tabagariluka32@gmail.com>
2025-01-05 21:42:12 +0400

    l funish 4lesson working

---
+++ davalebaN4_switch.js/blank.js
@@ -0,0 +1,12 @@
+let empty =prompt("")
+let Notempet=empty.length
+
+const bigempet =()=>{
+    if(Notempet===0){
+        alert(`${Notempet} this string is blank!`)
+    }else {
+        alert(`${Notempet>0} this string is not blank!`)
+    }
+
+}
+bigempet()
