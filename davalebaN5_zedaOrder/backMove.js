2e0006c905fc7a0317352f91da1dc400d957a260
LukaTabagari2222 <tabagariluka32@gmail.com>
2025-01-13 19:32:51 +0400

    as

--- davalebaN5_zedaOrder/backMove.js
+++ davalebaN5_zedaOrder/backMove.js
@@ -1,12 +1,12 @@
 
-// let aRreinumber=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-// let Ereyisnumber=aRreinumber.length
+let aRreinumber=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+let Ereyisnumber=aRreinumber.length
 
-// while(Ereyisnumber!=0){
-//   console.log(Ereyisnumber)
-//   Ereyisnumber=Ereyisnumber-1
+while(Ereyisnumber!=0){
+  console.log(Ereyisnumber)
+  Ereyisnumber=Ereyisnumber-1
 
-// }
+}
 
 // let aRreinumber=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,
 //     1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,
