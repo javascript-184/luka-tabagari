fef31157d3f8134b6f478c14bb1d037c4ff4a361
LukaTabagari2222 <tabagariluka32@gmail.com>
2025-01-17 20:08:27 +0400

    finiSh

---
+++ davalebaN5_zedaOrder/EmailBlock.js
@@ -0,0 +1,33 @@
+let EmailAdres=prompt("Enter gemeil:!")
+
+function emailEd(mail){
+    let [name,last]=mail.split("@");
+    let [firstName,lasTname]=name.split(".")
+    let deleteLastname= ".".repeat(lasTname.length)
+    
+    return `${firstName}${deleteLastname}${last}`;
+}
+alert(emailEd(EmailAdres))
+
+// კითხვა როგრო გვაკეთო ეს რომ ნებისმიერ ემილზე იმუშაოს
+
+
+
+
+ //განმარტება ფუნქციიას ვაწვდი პარამეტრს რადგან უფრო მოქნილი იყოს შემდეგ როგროც ვიცით splice 
+ // იყენებს მასივებს შესაბამსად  ვიყენებ მასივს    let [name,last]=mail.split("@"); ვეუბნები რომ სადაც @-იქნება
+ //ის ადგილები გახლიჩოს შედეგად 2 ად ყოფს    ანუ  name === "giorgi.beridze"
+ // last === "yahoo.com" შემდეგ ამ ცვლადით ვეუბნები რომ შევიდეს name-ში let [firstName,lasTname]=name.split(".") 
+ //ანუ  name==="giorgi.beridze" და აქ სადაც იქნებ .-წერტილი გახლიჩოს ანუ 2 ვყოფ შემდეგ ამ 2-ად გაყოფილს
+ //    let deleteLastname= ".".repeat(lasTname.length) ვდებ ცვლადში სადაც .-ვუწერ და repeat ვამრავლებ თავის თავზე
+ //რამდენიცაა გვარშ და      return `${firstName}${deleteLastname}${last}`; ვაბრუნებ ამ გაცხრილულ მონაცემებს
+
+
+
+
+
+
+
+
+
+
--- davalebaN5_zedaOrder/stRingorMas.js
+++ davalebaN5_zedaOrder/stRingorMas.js
@@ -1,9 +1,71 @@
-let reSult = "as bas"
+let reSult = prompt("")
 function stringToArray(testString) {
     let resulTmath = Array.from(testString)
-    console.log(resulTmath)
+    alert(resulTmath)
 }
 stringToArray(reSult)
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // მომხმარებლის მონაცემები
+// function createUserProfile(name, age, job, hobbies) {
+//     // ამ ფუნქციაში ქმნის მომხმარებლის ობიექტს
+//     let userProfile = {
+//         name: name,
+//         age: age,
+//         job: job,
+//         hobbies: hobbies
+//     };
+
+//     // დამატებითი ფუნქცია მომხმარებლის ასაკის შესამოწმებლად
+//     userProfile.displayProfile= function() {
+//         if (this.age < 18) {
+//             console.log(`${this.name} - ასაკი შესაბამს არაა!`);
+//         } else {
+//             console.log(`მომხმარებლის სახელი: ${this.name}`);
+//             console.log(`მომხმარებლის ასაკი: ${this.age}`);
+//             console.log(`მომხმარებლის სამუშაო: ${this.job}`);
+//             console.log(`ჰობი: ${this.hobbies.join(", ")}`);
+//         }
+//     };
+
+//     return userProfile;  // ობიექტი დავაბრუნოთ
+// }
+
+// // ახალი მომხმარებლის შექმნა
+// let user1 = createUserProfile("ანა", 16, "სერვისის მენეჯერი", ["მუსიკა", "პარალელური სწავლა"]);
+// let user2 = createUserProfile("გიორგი", 28, "დეველოპერი", ["პროგრამირება", "პოლიტიკა"]);
+// let user4 = createUserProfile("liuka", 28, "დეველოპერი", ["პროგრამირება", "პოლიტიკა"]);
+// let user5 = createUserProfile("gui", 28, "დეველოპერი", ["პროგრამირება", "პოლიტიკა"]);
+
+
+
+// // მომხმარებლის პროფილის ჩვენება
+// user1.displayProfile();  // ანას პროფილი
+// user2.displayProfile(); 
+// user4.displayProfile();
+// user5.displayProfile(); // გიორგის პროფილი
