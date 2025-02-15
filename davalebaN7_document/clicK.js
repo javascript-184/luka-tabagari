288f73eefbd77a87b0f375c78a3241796e6568ff
LukaTabagari2222 <tabagariluka32@gmail.com>
2025-02-16 01:49:45 +0400

    finish first

---
+++ davalebaN7_document/clicK.js
@@ -0,0 +1,54 @@
+window.addEventListener("DOMContentLoaded",()=>{
+    let submitBtn=document.getElementById("submitBtn")
+    submitBtn.addEventListener("click",submitBtnFuncshen)
+})
+let clickCount = 0;
+
+function submitBtnFuncshen() {
+    let img1 = document.getElementById("img1");
+    let img2=document.getElementById("img2")
+  clickCount++
+  if (clickCount<5){
+    if (img1.style.width === "10%") {
+        img1.style.width = "50%";
+    } else if (img1.style.width === "50%") {
+        img1.style.width = "80%";
+    } else if (img1.style.width === "70%") {
+        img1.style.width = "75%";
+    } else if (img1.style.width === "80%") {
+        img1.style.width = "85%";
+    }
+     else {
+        img1.style.width = "10%";
+        
+    }
+
+}
+else{
+    img1.style.display = "none";
+    img2.style.display = "block";
+} 
+}
+
+
+
+
+
+
+
+
+
+
+// window.addEventListener(`DOMContentLoaded`, () => {
+//     let img1 = document.getElementById("img1");
+//     let img2 = document.getElementById("img2");
+ 
+//     // თავიდან მეორე ფოტო უნდა დაიმალოს
+//     img2.style.display = "none";
+ 
+//     img1.addEventListener("click", () => {
+//        img1.style.display = "none";  // პირველი სურათი იმალება
+//        img2.style.display = "block"; // მეორე სურათი ჩნდება
+//     });
+//  });
+ 
