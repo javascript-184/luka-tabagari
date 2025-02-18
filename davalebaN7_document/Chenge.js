9138255f71aa5c05242cac3b0f1b59af1fe66b47
LukaTabagari2222 <tabagariluka32@gmail.com>
2025-02-19 00:18:18 +0400

    a

--- davalebaN7_document/Chenge.js
+++ davalebaN7_document/Chenge.js
@@ -1,16 +1,16 @@
-// window.addEventListener("DOMContentLoaded",()=>{
-//     let changeColorBtn=document.querySelector(".changeColorBtn")
-//     changeColorBtn.addEventListener("click",cheColorBtnfuncshn)
+window.addEventListener("DOMContentLoaded",()=>{
+    let changeColorBtn=document.querySelector(".changeColorBtn")
+    changeColorBtn.addEventListener("click",cheColorBtnfuncshn)
    
 
-// });
-// const colors=["black","green","red","purple","blue","yellow","orenge"];
-// let colorIndex = 0;
+});
+const colors=["black","green","red","purple","blue","yellow","orenge"];
+let colorIndex = 0;
 
 
-// function cheColorBtnfuncshn(){
-//     let text=document.getElementById("text")
-//     text.style.color = colors[colorIndex];
-//     colorIndex = (colorIndex + 1) % colors.length;
+function cheColorBtnfuncshn(){
+    let text=document.getElementById("text")
+    text.style.color = colors[colorIndex];
+    colorIndex = (colorIndex + 1) % colors.length;
     
-// }
+}
