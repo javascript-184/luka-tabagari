9815ea2d2af24ff4afb5e9ef3c5db15b90542535
LukaTabagari2222 <tabagariluka32@gmail.com>
2025-06-30 23:45:35 +0400

    finish lide

--- davalebaN11_slider/slider.js
+++ davalebaN11_slider/slider.js
@@ -1,4 +1,14 @@
-document.getElementById("b").addEventListener("click",fristboy)
-function fristboy(){
-    document.getElementById("b").style.backgroundColor="red"
+let slide = document.getElementsByClassName("slide");
+let index = 0;
+function setintevale(){
+    for(let i=0; i<slide.length; i++){
+        slide[i].style.display = "none";
+    }
+    slide[index].style.display="block"
+   index++
+  if(index>=slide.length){
+    index=0
+  }
 }
+setintevale()
+setInterval(setintevale,3000)
