7fa59351121665baa7efb49e9dfc148b6628dfab
LukaTabagari2222 <tabagariluka32@gmail.com>
2025-08-13 23:54:55 +0400

    as

--- davalebaN12_regex/fost.js
+++ davalebaN12_regex/fost.js
@@ -11,21 +11,21 @@ function fostafuncshen() {
   let textand = document.getElementById("textand");
   let input = document.getElementById("input").value.trim();
   const found = cities.find((cit) => cit.code === input);
-    textand.innerHTML = "";
-    let p = document.createElement("p")
+  textand.innerHTML = "";
+  let p = document.createElement("p");
 
   if (!/^\d+$/.test(input)) {
- p.textContent = "უნდა შეიცავდეს რიცხვს";
+    p.textContent = "უნდა შეიცავდეს რიცხვს";
     p.style.color = "red";
   } else if (input.length !== 4) {
     p.textContent = "შეიყვანეთ მხოლოდ 4 რიცხვი";
     p.style.color = "red";
     p.style.textShadow = "2px 2px 3px black";
   } else if (found) {
-  p.textContent = found.name;
+    p.textContent = found.name;
     p.style.color = found.color;
-    p.classList.add(found.className)  } 
-    else {
+    p.classList.add(found.className);
+  } else {
     p.textContent = "მიღებულია";
     p.style.color = "green";
     p.style.textShadow = "2px 3px 2px red";
